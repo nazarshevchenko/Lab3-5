0f2864c8fd9ba9fdbf1b63a04d28261da142cce2
Shevchenko Nazar <45729327+nazarshevchenko@users.noreply.github.com>
2020-10-25 23:01:21 +0200

    first

--- main/main/settings.py
+++ main/main/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hero',
+    'create',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
